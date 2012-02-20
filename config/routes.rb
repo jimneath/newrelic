7437150bfae11084f669bff4f5f78abc15ee0e91
Jim Neath <jimneath@googlemail.com>
2012-02-20 13:55:02 +0000

    sfdd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Newrelic::Application.routes.draw do
+  resources :apples
+  resources :bananas
+  resources :carrots
+  resources :dragonfruits
+  resources :eggplants
+  resources :fennels
+  resources :grapefruits
+  resources :horseradishes
+  resources :inga_beans
+  
   root to: 'debug#index'
 end
