1cc072e9b40594d1480ba2afb0a64cac48623a98
Jim Neath <jimneath@googlemail.com>
2012-02-20 11:26:04 +0000

    middleware

--- config.ru
+++ config.ru
@@ -1,7 +1,9 @@
 # setup
+require './middleware/middleware_start.rb'
 require './app'
 
 # middleware
+use NewRelic::MiddlewareStart
 use Rack::ShowExceptions
 
 # app
---
+++ middleware/middleware_start.rb
@@ -0,0 +1,12 @@
+module NewRelic
+  class MiddlewareStart
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      env["HTTP_X_MIDDLEWARE_START"] = "t=#{(Time.now.to_f * 1000000).to_i}"
+      @app.call(env)
+    end
+  end
+end
