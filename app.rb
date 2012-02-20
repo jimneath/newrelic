cacc7b59a06df254b61f36c7722d612dcf444809
Jim Neath <jimneath@googlemail.com>
2012-02-20 11:48:41 +0000

    middleware

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
     request_start = env['HTTP_X_REQUEST_START'][/\d+$/].to_i
     queue_start = env['HTTP_X_QUEUE_START'][/\d+$/].to_i
     middleware_start = env['HTTP_X_MIDDLEWARE_START'][/\d+$/].to_i
-    now = Time.now.to_i
+    now = (Time.now.to_f * 1000000).to_i
 
     @server_time = queue_start - request_start
     @queue_time = middleware_start - queue_start
