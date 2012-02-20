54acd93844f391f6d7c40fc59739dc905050e125
Jim Neath <jimneath@googlemail.com>
2012-02-20 11:44:49 +0000

    middleware

--- app.rb
+++ app.rb
@@ -10,6 +10,15 @@ class App < Sinatra::Base
 
   # routes
   get '/' do
+    request_start = headers['HTTP_X_REQUEST_START'][/\d+$/].to_i
+    queue_start = headers['HTTP_X_QUEUE_START'][/\d+$/].to_i
+    middleware_start = headers['HTTP_X_MIDDLEWARE_START'][/\d+$/].to_i
+    now = Time.now.to_i
+
+    @server_time = queue_start - request_start
+    @queue_time = middleware_start - queue_start
+    @middleware_time = now - middleware_start
+    
     erb :index
   end
 end
